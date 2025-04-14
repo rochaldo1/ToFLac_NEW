037128e3eafde1c17533e5887ec4b7c88cf350a3
rochaldo1 <xusainow.artur2018@yandex.ru>
2025-04-14 22:36:50 +0700

    basic View, MainVM and a little bit of Model

--- ToFLac_NEW/Model/Enums/TokenEnum.cs
+++ ToFLac_NEW/Model/Enums/TokenEnum.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToFLac_NEW.Model
+﻿namespace ToFLac_NEW.Model
 {
     public enum TokenEnum
     {
