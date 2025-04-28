47e2535015f5ead0a2d9a21d8db87188ce754f3a
rochaldo1 <xusainow.artur2018@yandex.ru>
2025-04-28 17:22:02 +0700

    Lexer fix for BrokenIdentifiers
    FUCK

--- ToFLac_NEW/Model/Lexer/Lexer.cs
+++ ToFLac_NEW/Model/Lexer/Lexer.cs
@@ -1,232 +1,240 @@
 ﻿using System.Text.RegularExpressions;
-using ToFLac_NEW.Model.Lexer;
 
-public class Lexer
+namespace ToFLac_NEW.Model.Lexer
 {
-    private const string pattern =
-        @"(int|i[^n\w\s]?nt|in[^t\w\s]?t|i[^n\w\s]?n[^t\w\s]?t)|" +
-        @"(float|f[^l\w\s]?loat|fl[^o\w\s]?oat|flo[^a\w\s]?at|floa[^t\w\s]?t|f[^l\w\s]?l[^o\w\s]?o[^a\w\s]?a[^t\w\s]?t)|" +
-        @"(double|d[^o\w\s]?ouble|do[^u\w\s]?uble|dou[^b\w\s]?ble|doub[^l\w\s]?le|doubl[^e\w\s]?e|d[^o\w\s]?o[^u\w\s]?u[^b\w\s]?b[^l\w\s]?l[^e\w\s]?e)|" +
-        @"(char|c[^h\w\s]?har|ch[^a\w\s]?ar|cha[^r\w\s]?r|c[^h\w\s]?h[^a\w\s]?a[^r\w\s]?r)|" +
-        @"(new|n[^e\w\s]?ew|ne[^w\w\s]?w|n[^e\w\s]?e[^w\w\s]?w)|" +
-        @"\*|" +
-        @"=|\(|\)|;|" +
-        @"[a-zA-Z][a-zA-Z0-9]*|" +
-        @"\s+|" +
-        @"[^\s]";
-
-    public List<Token> GetLexemes(string text)
+    public class Lexer
     {
-        List<Token> tokens = new();
-        string[] lines = text.Split('\n');
+        private const string pattern =
+            @"(int|i[^n\w\s]?nt|in[^t\w\s]?t|i[^n\w\s]?n[^t\w\s]?t)|" +
+            @"(float|f[^l\w\s]?loat|fl[^o\w\s]?oat|flo[^a\w\s]?at|floa[^t\w\s]?t|f[^l\w\s]?l[^o\w\s]?o[^a\w\s]?a[^t\w\s]?t)|" +
+            @"(double|d[^o\w\s]?ouble|do[^u\w\s]?uble|dou[^b\w\s]?ble|doub[^l\w\s]?le|doubl[^e\w\s]?e|d[^o\w\s]?o[^u\w\s]?u[^b\w\s]?b[^l\w\s]?l[^e\w\s]?e)|" +
+            @"(char|c[^h\w\s]?har|ch[^a\w\s]?ar|cha[^r\w\s]?r|c[^h\w\s]?h[^a\w\s]?a[^r\w\s]?r)|" +
+            @"(new|n[^e\w\s]?ew|ne[^w\w\s]?w|n[^e\w\s]?e[^w\w\s]?w)|" +
+            @"\*|" +
+            @"=|\(|\)|;|" +
+            @"[a-zA-Z][a-zA-Z0-9]*|" +
+            @"\s+|" +
+            @"[^\s]";
 
-        for (int lineNum = 0; lineNum < lines.Length; lineNum++)
+        public List<Token> GetLexemes(string text)
         {
-            string line = lines[lineNum];
-            int pos = 0;
+            List<Token> tokens = new();
+            string[] lines = text.Split('\n');
 
-            while (pos < line.Length)
+            for (int lineNum = 0; lineNum < lines.Length; lineNum++)
             {
-                Match match = Regex.Match(line.Substring(pos), @"^(" + pattern + ")");
-                if (!match.Success)
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        pos,
-                        pos + 1,
-                        $"Недопустимый символ: '{line[pos]}'",
-                        line[pos].ToString()
-                    ));
-                    pos++;
-                    continue;
-                }
+                string line = lines[lineNum];
+                int pos = 0;
 
-                string value = match.Value;
-                int start = pos;
-                pos += value.Length;
-
-                if (Regex.IsMatch(value, @"^int$|^float$|^double$|^char$"))
+                while (pos < line.Length)
                 {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Тип: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^new$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Ключевое слово: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^\*$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Указатель: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^=$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Знак равенства: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^\($"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Открывающая скобка: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^\)$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Закрывающая скобка: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^;$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Точка с запятой: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^\s+$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Пробел: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^[a-zA-Z][a-zA-Z0-9]*$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Идентификатор: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^i[^n\w\s]?nt$|^in[^t\w\s]?t$|^i[^n\w\s]?n[^t\w\s]?t$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Сломанный int: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^f[^l\w\s]?loat$|^fl[^o\w\s]?oat$|^flo[^a\w\s]?at$|^floa[^t\w\s]?t$|^f[^l\w\s]?l[^o\w\s]?o[^a\w\s]?a[^t\w\s]?t$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Сломанный float: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^d[^o\w\s]?ouble$|^do[^u\w\s]?uble$|^dou[^b\w\s]?ble$|^doub[^l\w\s]?le$|^doubl[^e\w\s]?e$|^d[^o\w\s]?o[^u\w\s]?u[^b\w\s]?b[^l\w\s]?l[^e\w\s]?e$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Сломанный double: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^c[^h\w\s]?har$|^ch[^a\w\s]?ar$|^cha[^r\w\s]?r$|^c[^h\w\s]?h[^a\w\s]?a[^r\w\s]?r$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Сломанный char: '{value}'",
-                        value
-                    ));
-                }
-                else if (Regex.IsMatch(value, @"^n[^e\w\s]?ew$|^ne[^w\w\s]?w$|^n[^e\w\s]?e[^w\w\s]?w$"))
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Сломанный new: '{value}'",
-                        value
-                    ));
-                }
-                else
-                {
-                    tokens.Add(new Token(
-                        lineNum + 1,
-                        start,
-                        pos,
-                        $"Недопустимый токен: '{value}'",
-                        value
-                    ));
-                }
-            }
-        }
-
-        return MergeBrokenIdentifiers(tokens);
-    }
-
-    private List<Token> MergeBrokenIdentifiers(List<Token> tokens)
-    {
-        List<Token> mergedTokens = new();
-        int i = 0;
-
-        while (i < tokens.Count)
-        {
-            if (tokens[i].TypeCode == TokenType.Identifier)
-            {
-                int startIndex = i;
-                int endIndex = i;
-
-                while (endIndex + 1 < tokens.Count && tokens[endIndex + 1].TypeCode == TokenType.Invalid)
-                {
-                    endIndex++;
-                    if (endIndex + 1 < tokens.Count && tokens[endIndex + 1].TypeCode == TokenType.Identifier)
+                    Match match = Regex.Match(line.Substring(pos), @"^(" + pattern + ")");
+                    if (!match.Success)
                     {
-                        endIndex++;
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            pos,
+                            pos + 1,
+                            $"Недопустимый символ: '{line[pos]}'",
+                            line[pos].ToString()
+                        ));
+                        pos++;
+                        continue;
+                    }
+
+                    string value = match.Value;
+                    int start = pos;
+                    pos += value.Length;
+
+                    if (Regex.IsMatch(value, @"^int$|^float$|^double$|^char$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Тип: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^new$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Ключевое слово: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^\*$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Указатель: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^=$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Знак равенства: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^\($"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Открывающая скобка: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^\)$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Закрывающая скобка: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^;$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Точка с запятой: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^\s+$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Пробел: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^[a-zA-Z][a-zA-Z0-9]*$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Идентификатор: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^i[^n\w\s]?nt$|^in[^t\w\s]?t$|^i[^n\w\s]?n[^t\w\s]?t$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Сломанный int: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^f[^l\w\s]?loat$|^fl[^o\w\s]?oat$|^flo[^a\w\s]?at$|^floa[^t\w\s]?t$|^f[^l\w\s]?l[^o\w\s]?o[^a\w\s]?a[^t\w\s]?t$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Сломанный float: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^d[^o\w\s]?ouble$|^do[^u\w\s]?uble$|^dou[^b\w\s]?ble$|^doub[^l\w\s]?le$|^doubl[^e\w\s]?e$|^d[^o\w\s]?o[^u\w\s]?u[^b\w\s]?b[^l\w\s]?l[^e\w\s]?e$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Сломанный double: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^c[^h\w\s]?har$|^ch[^a\w\s]?ar$|^cha[^r\w\s]?r$|^c[^h\w\s]?h[^a\w\s]?a[^r\w\s]?r$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Сломанный char: '{value}'",
+                            value
+                        ));
+                    }
+                    else if (Regex.IsMatch(value, @"^n[^e\w\s]?ew$|^ne[^w\w\s]?w$|^n[^e\w\s]?e[^w\w\s]?w$"))
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Сломанный new: '{value}'",
+                            value
+                        ));
+                    }
+                    else
+                    {
+                        tokens.Add(new Token(
+                            lineNum + 1,
+                            start,
+                            pos,
+                            $"Недопустимый токен: '{value}'",
+                            value
+                        ));
                     }
                 }
+            }
 
-                if (endIndex > startIndex)
+            return MergeBrokenIdentifiers(tokens);
+        }
+
+        private List<Token> MergeBrokenIdentifiers(List<Token> tokens)
+        {
+            List<Token> mergedTokens = new();
+            int i = 0;
+
+            while (i < tokens.Count)
+            {
+                if (tokens[i].TypeCode == TokenType.Identifier)
                 {
-                    Token mergedToken = Token.MergeTokens(tokens, startIndex, endIndex);
-                    mergedTokens.Add(mergedToken);
-                    i = endIndex + 1;
+                    int startIndex = i;
+                    int endIndex = i;
+
+                    if (endIndex + 2 < tokens.Count &&
+                        tokens[endIndex + 1].TypeCode == TokenType.Invalid &&
+                        tokens[endIndex + 2].TypeCode == TokenType.Identifier)
+                    {
+                        endIndex += 2;
+
+                        Token mergedToken = Token.MergeTokens(tokens, startIndex, endIndex);
+                        mergedTokens.Add(mergedToken);
+                        i = endIndex + 1;
+
+                        if (i < tokens.Count && tokens[i].TypeCode == TokenType.Invalid)
+                        {
+                            mergedTokens.Add(tokens[i]);
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        mergedTokens.Add(tokens[i]);
+                        i++;
+                    }
                 }
                 else
                 {
@@ -234,13 +242,8 @@ public class Lexer
                     i++;
                 }
             }
-            else
-            {
-                mergedTokens.Add(tokens[i]);
-                i++;
-            }
-        }
 
-        return mergedTokens;
+            return mergedTokens;
+        }
     }
 }
